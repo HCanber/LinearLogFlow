25330f4634aea15405d1aca9535bf65a4a45d814
HCanber <hakan_github@canberger.se>
2015-05-08 09:04:45 +0200

    Bump version to 0.2.3

--- src/LinearLogFlow/Properties/AssemblyInfo.cs
+++ src/LinearLogFlow/Properties/AssemblyInfo.cs
@@ -21,15 +21,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("94ed9923-bd50-4a70-b0cb-4b705aa5eb5a")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+
+[assembly: AssemblyVersion("0.2.4.0")]
+[assembly: AssemblyFileVersion("0.2.4.*")]
+[assembly: AssemblyInformationalVersion("0.2.4")]
